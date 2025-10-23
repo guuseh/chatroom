be3ff5b2560fb234d453220903d66495e2ef702c
guuseh <guus-99@live.nl>
2025-10-23 12:35:17 +0300

    what/...

--- src/pages/demonlovers.jsx
+++ src/pages/demonlovers.jsx
@@ -129,7 +129,7 @@ const Demonlovers = ({setProjectCounter, visitPage}) => {
   const props = {
     height: '80vh',
     width: '80vw',
-    fisheye: 0.25,
+    fisheye: 0.5,
     navbar: [],
     defaultZoomLvl: 0,
     src: "/img/02/1.png",
