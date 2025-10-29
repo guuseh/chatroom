3643bb4095445325867e2090ec842cf0982aa86e
guuseh <guus-99@live.nl>
2025-10-29 17:10:24 +0200

    unlock noura

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ import Sotce from "./pages/sotce.jsx" // video ROOM 10
 
 function App() {
   const urls = ["/01", "/02", "/03", "/04", "/05",
-                 "/07", "/08", "/09", "/10",]
+                 "/06", "/07", "/08", "/09", "/10",]
   const [shuffledUrls, setShuffledUrls] = useState([]) // will only contain urls
   const [projectCounter, setProjectCounter] = useState(0)
   const [shuffledDivs, setShuffledDivs] = useState([]) // will contain also empty 0 divs
-  const [array, setArray] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "/06", "/11", "/12"])
+  const [array, setArray] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "/11", "/12"])
   const [visited, setVisited] = useState({count: 0})
   const [showObjects, setShowObjects] = useState(false)
 
