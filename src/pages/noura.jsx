985c5d78b96dbf2c86a3133bd41ce8bf97474554
guuseh <guus-99@live.nl>
2025-10-30 14:38:58 +0200

    hover state noura files

--- src/pages/noura.jsx
+++ src/pages/noura.jsx
@@ -50,17 +50,30 @@ const Noura = ({setProjectCounter, visitPage}) => {
     const [left, setLeft] = useState(false)
     const [center, setCenter] = useState(false)
     const [right, setRight] = useState(false)
+    const [leftH, setLeftH] = useState(true)
+    const [centerH, setCenterH] = useState(true)
+    const [rightH, setRightH] = useState(true)
 
     const fileVariants = {
       down: (i) => ({
         y: 0,
+        zIndex: i == 'l' ? 3 : i == 'c' ? 4 : 5,
         scale: 1
       }),
       up: (i) => ({
-        y: "-100%",
+        y: "-105%",
+        zIndex: i == 'l' ? 10 : i == 'c' ? 11 : 12,
         scale: 1.2
+      }),
+      hover: (i) => ({
+        y: i == 'l' && !left ? "-5%" 
+            : i == 'r' && !right ? "-5%" 
+            : i == "c" && !center ? "-5%" 
+            : null
       })
     }
+
+    // onAnimationComplete={(latest) => {latest == "start" && setStartType(true)}}
 
   return (
     <>
@@ -71,9 +84,9 @@ const Noura = ({setProjectCounter, visitPage}) => {
           <div id="noura-coverart" style={{height: "50vh", marginTop: "8vh"}}><img src="/img/06/coverart.png" style={{height: "100%"}}/></div>
           
           
-        <motion.div onClick={() => setRight(!right)} className="noura-file" id="noura-right" variants={fileVariants} custom="l" animate={right ? "up" : "down"}><img src="/img/06/character1.png" style={{height: "100%"}}/></motion.div>
-        <motion.div onClick={() => setCenter(!center)} className="noura-file" id="noura-center" variants={fileVariants} custom="l" animate={center ? "up" : "down"}><img src="/img/06/character2.png" style={{height: "100%"}}/></motion.div>
-        <motion.div onClick={() => setLeft(!left)} className="noura-file" id="noura-left" variants={fileVariants} custom="l" animate={left ? "up" : "down"}><img src="/img/06/character3.png" style={{height: "100%"}}/></motion.div>
+        <motion.div onClick={() => {setRight(!right); right && setRightH(false)}} onAnimationComplete={(latest) => {latest == "down" && setRightH(true)}} whileHover={!right && rightH && "hover"} className="noura-file" id="noura-right" variants={fileVariants} custom="l" animate={right ? "up" : "down"}><img src="/img/06/character1.png" style={{height: "100%"}}/></motion.div>
+        <motion.div onClick={() => {setCenter(!center); center && setCenterH(false)}} onAnimationComplete={(latest) => {latest == "down" && setCenterH(true)}} whileHover={!center && centerH && "hover"} className="noura-file" id="noura-center" variants={fileVariants} custom="c" animate={center ? "up" : "down"}><img src="/img/06/character2.png" style={{height: "100%"}}/></motion.div>
+        <motion.div onClick={() => {setLeft(!left); left && setLeftH(false)}} onAnimationComplete={(latest) => {latest == "down" && setLeftH(true)}} whileHover={!left && leftH && "hover"} className="noura-file" id="noura-left" variants={fileVariants} custom="r" animate={left ? "up" : "down"}><img src="/img/06/character3.png" style={{height: "100%"}}/></motion.div>
          
         </motion.div>
       </div>
